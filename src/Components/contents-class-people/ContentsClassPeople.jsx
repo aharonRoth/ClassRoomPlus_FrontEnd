abd46050226cc9c68657a685905be9a75924bf60
ahron <a0548421610@gmail.com>
2024-07-03 17:58:56 +0300

    contentClassPeople

--- src/Components/contents-class-people/ContentsClassPeople.jsx
+++ src/Components/contents-class-people/ContentsClassPeople.jsx
@@ -5,45 +5,81 @@ import axios from 'axios';
 const ContentsClassPeople = ({ courseId }) => {
   const [error, setError] = useState(null);
   const [subscribed, setSubscribed] = useState([]);
+  const [theCourseTeacher, setTheCourseTeacher] = useState(null);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(`http://localhost:3000/courses/${courseId}`, { withCredentials: true });
+        console.log('Response data:', data);
 
-        const theCourseSubscriptions = data.courses.subscription;
+        if (data.course && data.course.userId) {
+          const TeacherId = data.course.userId;
+          console.log(TeacherId);
+          setTheCourseTeacher(TeacherId);
+        } else {
+          throw new Error('Teacher ID not found in the response');
+        }
 
-        const filtered = theCourseSubscriptions.map(subscription => subscription.userId);
+        if (data.courses && data.courses.subscription) {
+          const theCourseSubscriptions = data.courses.subscription;
+          const filtered = theCourseSubscriptions.map(subscription => subscription.userId);
+          setSubscribed(filtered);
+        } else {
+          throw new Error('Subscriptions not found in the response');
+        }
 
-        setSubscribed(filtered);
+        setLoading(false); 
+
       } catch (error) {
+        console.error('Error fetching data:', error.message);
         setError('There is an error');
+        setLoading(false); 
       }
     };
 
     fetchData();
   }, [courseId]);
 
+  console.log('theCourseTeacher state:', theCourseTeacher);
+
+  if (loading) {
+    return <p>Loading...</p>; 
+  }
+
   return (
     <div>
+      <h2> Teachers </h2>
+      {theCourseTeacher ? (
+        <div> {theCourseTeacher.firstName} {theCourseTeacher.lastName} </div>
+      ) : (
+        <p>Loading...</p>
+      )}
       <h2>Users List</h2>
-      {error && <p>{error}</p>}
       <ul>
         {subscribed.length > 0 ? (
           subscribed.map((user, index) => (
             <li key={index}>
-                {user ? `${user.firstName} ${user.lastName}` : 'No ID available'}
+              {user ? `${user.firstName} ${user.lastName}` : 'No ID available'}
             </li>
           ))
         ) : (
           <p>No users subscribed.</p>
         )}
       </ul>
+        {error && <p>{error}</p>}
     </div>
   );
 };
 
 export default ContentsClassPeople;
+
+
+
+
+
+
 
 
 
